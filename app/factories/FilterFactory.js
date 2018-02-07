2093ec67a78aacc589aa1078aafacead9af6c978
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-07 11:10:18 -0600

    added filter factory just to have it

--- app/factories/FilterFactory.js
+++ app/factories/FilterFactory.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict'; 
+
+  // Factory for getting term entered in search bar by user
+  angular.module("Datr").factory("FilterFactory", function () {
+      return {
+        term: ""
+      };
+  });
