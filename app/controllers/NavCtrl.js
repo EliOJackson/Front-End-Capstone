c3564d58fe16f90f1f04370251a41683f41d7ee6
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-12 10:29:05 -0600

    when user logs in, 'My Dates' will be able to redirect them to their user page

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,14 +1,15 @@
 "use strict";
 
 angular.module("Datr").controller("NavCtrl", function ($scope, AuthFactory) {
-
+    
     // LOGIN USER
     // call auth factory to log in user
     $scope.login = () => {
         AuthFactory.login()
             .then(() => {
                 console.log("Yay, logged in");
-            })
+                $scope.uid = firebase.auth().currentUser.uid;
+                })
             .catch(err => {
                 console.log("error", err);
             });
