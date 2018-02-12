0cbc79bb61d794c06206ffab80685d34386197f5
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-12 15:31:22 -0600

    set up rating factory, moved over rating functions to said factory

--- app/controllers/DateListCtrl.js
+++ app/controllers/DateListCtrl.js
@@ -1,53 +1,18 @@
 "use strict";
 
-angular.module("Datr").controller("DateListCtrl", function ($scope, DateFactory, $q, lodash) {
+angular.module("Datr").controller("DateListCtrl", function ($scope, DateFactory, $q, RatingFactory) {
     $scope.title = "Date List";
     $scope.saved = {};
 
     DateFactory.getAllDates()
         .then(data => {
             $scope.dates = data;
-            let promiseArray = []; // setting a promise array to push all of my calls to FB for Ratings
-            $scope.dates.forEach(date => {
-                let dateId = date.dateId;
-                let ratings = DateFactory.getDateRating(dateId);
-                promiseArray.push(ratings); //pushing all of the dates from get date rating in to the array
-
+            console.log($scope.dates, "huh??");
+            RatingFactory.rateDates(data)
+                .then(info => {
+                    console.log($scope.dates, "why???");
+                });
             });
-            return $q.all(promiseArray) // promise all on the array of the rating promises
-                .then((dates) => {
-                    dates.forEach(date => {
-                        let ratingArray = [];
-                        if (Object.keys(date).length > 0) { // if to filter out any objects returned without ratings
-                            let ratingKeys = Object.keys(date);
-                            ratingKeys.forEach(ratingKey => {
-                                ratingArray.push(parseInt(date[ratingKey].rating));
-                            });
-                            let ratingAverage = average(ratingArray); // averages each array of dates using the function below
-                            dateLoop(ratingAverage, date); //loops through the averaged ratings and adds them as a key on to their date
-                        }
-                    });
-                });
-
-            //internal function called on line 25
-            function average(array) {
-                let sum = array.reduce((b, a) => a += b);
-                let avg = (sum / array.length).toFixed(2);
-                return avg;
-            }
-            //internal function called on line 26
-            function dateLoop(rating, dates) {
-                let datesArrays = Object.entries(dates);
-                datesArrays.forEach(dateArray => {
-                    let dateId = dateArray[1].dateId;
-                    for (let i = 0; i < $scope.dates.length; i++) {
-                        if (dateId === $scope.dates[i].dateId) {
-                            $scope.dates[i].rating = rating;
-                        }
-                    }
-                });
-            }
-        });
 
     $scope.saveDate = function () {
         $scope.saved.dateId = this.date.dateId;
---
+++ app/factories/RatingFactory.js
@@ -0,0 +1,68 @@
+"use strict";
+
+angular.module("Datr").factory("RatingFactory", function (FBUrl, $q, $http) {
+
+    function rateDates(dates) {
+        return $q((resolve, reject) => {
+            let allDates = dates;
+            let promiseArray = []; // setting a promise array to push all of my calls to FB for Ratings
+            dates.forEach(date => {
+                let dateId = date.dateId;
+                let ratings = getDateRating(dateId);
+                promiseArray.push(ratings); //pushing all of the dates from get date rating in to the array
+
+            });
+            return $q.all(promiseArray) // promise all on the array of the rating promises
+                .then((dates) => {
+                    dates.forEach(date => {
+                        let ratingArray = [];
+                        if (Object.keys(date).length > 0) { // if to filter out any objects returned without ratings
+                            let ratingKeys = Object.keys(date);
+                            ratingKeys.forEach(ratingKey => {
+                                ratingArray.push(parseInt(date[ratingKey].rating));
+                            });
+                            let ratingAverage = average(ratingArray); // averages each array of dates using the function below
+                            dateLoop(ratingAverage, date, allDates); //loops through the averaged ratings and adds them as a key on to their date
+                        }
+                    });
+                    resolve(dates);
+                });
+
+        });
+    }
+    //internal function called on line 25
+    function average(array) {
+        let sum = array.reduce((b, a) => a += b);
+        let avg = (sum / array.length).toFixed(2);
+        return avg;
+    }
+    //internal function called on line 26
+    function dateLoop(rating, dates, allDates) {
+        let datesArrays = Object.entries(dates);
+        console.log(allDates, "datesArrays");
+        datesArrays.forEach(dateArray => {
+            let dateId = dateArray[1].dateId;
+            allDates.forEach(date => {
+                if (dateId === date.dateId) {
+                    date.rating = rating;
+                }
+            });
+        });
+    }
+
+    function getDateRating(dateId) {
+        return $q((resolve, reject) => {
+            $http
+                .get(`${FBUrl}rating.json?orderBy="dateId"&equalTo="${dateId}"`)
+                .then(({ data }) => {
+                    resolve(data);
+                })
+                .catch(error => {
+                    console.log("getDate rating", error);
+                });
+        });
+    }
+
+    return { rateDates, average, dateLoop, getDateRating };
+
+});
