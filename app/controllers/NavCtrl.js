2c5ac2a0d9d37076f0e2b3475ee65e909e98066f
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-12 13:33:19 -0600

    auth changes to make sure user is logged in to view their saved dates page

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -2,13 +2,22 @@
 
 angular.module("Datr").controller("NavCtrl", function ($scope, AuthFactory) {
     
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            $scope.$apply($scope.user = true);
+            $scope.uid = firebase.auth().currentUser.uid;
+            
+        } else {
+            $scope.$apply($scope.user = false);
+        }
+    });
     // LOGIN USER
     // call auth factory to log in user
     $scope.login = () => {
         AuthFactory.login()
             .then(() => {
                 console.log("Yay, logged in");
-                $scope.uid = firebase.auth().currentUser.uid;
+                // $scope.uid = firebase.auth().currentUser.uid;
                 })
             .catch(err => {
                 console.log("error", err);
@@ -20,6 +29,9 @@ angular.module("Datr").controller("NavCtrl", function ($scope, AuthFactory) {
     $scope.logout = () => {
         AuthFactory.logout()
             .then(() => {
+                $scope.$apply($scope.user = false);
+                $scope.uid = 0;
+                
                 console.log("You is gone");
             });
     };
