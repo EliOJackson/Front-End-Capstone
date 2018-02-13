ad72c987d0e8ba87968cce5fd13bef59e251fa5d
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-13 10:53:19 -0600

    now reloads the page after a delete

--- app/controllers/UserDatesCtrl.js
+++ app/controllers/UserDatesCtrl.js
@@ -3,40 +3,59 @@
 angular.module("Datr").controller("UserDatesCtrl", function ($scope, DateFactory, $routeParams, $q, RatingFactory, $http, FBUrl) {
     $scope.title = "User's Dates";
     $scope.dates = [];
- 
+
     DateFactory.getSavedDates($routeParams.uid)
         .then(data => {
             let promiseArray = [];
             let savedDateIds = Object.entries(data);
             savedDateIds.forEach(savedDateId => {
                 savedDateId[1].fbKey = savedDateId[0];
-                console.log('savedDateId',savedDateId[0]);
-                console.log("test",savedDateId[1]);
+                console.log('savedDateId', savedDateId[0]);
+                console.log("test", savedDateId[1]);
                 let saved = DateFactory.datesToPrint(savedDateId[1].dateId, savedDateId[1].fbKey);
                 promiseArray.push(saved);
-                
-                
+
+
 
             });
             return $q.all(promiseArray)
-                .then((dates ) => {
+                .then((dates) => {
                     dates.forEach(date => {
-                    $scope.dates.push(date.data);     
-                    RatingFactory.rateDates($scope.dates);
+                        $scope.dates.push(date.data);
+                        RatingFactory.rateDates($scope.dates);
+                    });
                 });
-            });
         });
 
     $scope.delete = (savedKey) => {
         return $q((resolve, reject) => {
             $http
-            .delete(`${FBUrl}/saved/${savedKey}.json`
-        );
-            console.log("this", savedKey); 
-            // $scope.saved.dateId = this.date.dateId;
-            // $scope.saved.uid = firebase.auth().currentUser.uid;
-            console.log('this.date.dateId', this.date);
-            console.log('firebase.auth().currentUser.uid', firebase.auth().currentUser.uid);
+                .delete(`${FBUrl}/saved/${savedKey}.json`
+                )
+                .then(() => {
+                    $scope.dates = [];
+                    resolve();
+                    DateFactory.getSavedDates($routeParams.uid)
+                        .then(data => {
+                            let promiseArray = [];
+                            let savedDateIds = Object.entries(data);
+                            savedDateIds.forEach(savedDateId => {
+                                savedDateId[1].fbKey = savedDateId[0];
+                                let saved = DateFactory.datesToPrint(savedDateId[1].dateId, savedDateId[1].fbKey);
+                                promiseArray.push(saved);
+
+
+
+                            });
+                            return $q.all(promiseArray)
+                                .then((dates) => {
+                                    dates.forEach(date => {
+                                        $scope.dates.push(date.data);
+                                        RatingFactory.rateDates($scope.dates);
+                                    });
+                                });
+                        });
+                });
         });
     };
 });
